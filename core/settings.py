b7d7e96c972ce60afb0ea4dbcfb0b9d8df25090e
Eduardo Sanchez <80008158+lorf543@users.noreply.github.com>
2025-02-05 08:40:23 -0400

    m

--- core/settings.py
+++ core/settings.py
@@ -34,7 +34,6 @@ ENVIRONMENT = 'production'
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-ENVIRONMENT = os.getenv('ENVIRONMENT', default='production')
 
 DEBUG = os.getenv('DEBUG')
 
